658c72cb549a4a6621bbd8194a20804c71a559fa
Viraj Chandra <virajchandra51@gmail.com>
2025-10-04 14:09:32 +0530

    minor

--- 900/14 - AvtoBus.cpp
+++ 900/14 - AvtoBus.cpp
@@ -12,7 +12,7 @@ int main()
             cout << -1 << '\n';
         else
         {
-            // Using a different formula for ceil and floor to avoid floating point operations
+            // Using a different formula for ceil to avoid floating point operations
             // in C++20 (GCC 13-64) version 
             long long min_buses = (n + 5) / 6; // integer ceil of n/6
             long long max_buses = n / 4;       // integer floor of n/4
